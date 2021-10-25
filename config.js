31d318ab9c69cf55d8b44f094a834cd1a311338a
manhyeunhi7721 <92881087+manhyeunhi7721@users.noreply.github.com>
2021-10-25 20:01:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     introTitle: 'Em à',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim anh lẻ loi 
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên',
