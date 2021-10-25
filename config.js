9afcb7bbc227c2bcef2a139b561cb5a50c28e3c3
manhyeunhi7721 <92881087+manhyeunhi7721@users.noreply.github.com>
2021-10-25 19:58:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Vì anh Thế đẹp zai <3',
-    mess: 'Anh biết mà 🥰. Yêu Em nhiều nhiều 😘😘',
+    reply: 'Vì anh là người đàn ông đã mang lại cho em hạnh fuck <3',
+    mess: 'Anh biết mà. Yêu Em nhiều nhiều 😘😘',
     messDesc: 'Nhắn tin cho anh nhé baby😘',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/minhthe.bui.161' //link mess của các bạn. VD: https://m.me/nam.nodemy
